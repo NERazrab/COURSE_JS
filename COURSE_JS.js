b5427d93fb23f124cba1751d8fc5c6cdfc9c813b
Edward <edward.razrab@gmail.com>
2023-10-10 21:15:31 +0300

    Second commit

--- COURSE_JS.js
+++ COURSE_JS.js
@@ -44,6 +44,54 @@
 
 // ТРЕТИЙ ВАРИАНТ ЗАПИСИ:
 
+// let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// let personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false,
+// };
+
+// let a = prompt("Один из последних просмотренных фильмов?", "");
+// let b = +prompt("На сколько оцените его?", "");
+// let c = prompt("Один из последних просмотренных фильмов?", "");
+// let d = +prompt("На сколько оцените его?", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// ________________________________________________
+
+// ПРАКТИКА, ЧАСТЬ 2
+
+// let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+
+// let personalMovieDB = {
+// 	count: numberOfFilms,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false,
+// };
+
+// let a = prompt("Один из последних просмотренных фильмов?", "");
+// let b = +prompt("На сколько оцените его?", "");
+// let c = prompt("Один из последних просмотренных фильмов?", "");
+// let d = +prompt("На сколько оцените его?", "");
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+// console.log(personalMovieDB);
+
+// ________________________________________________
+
+// ПРАКТИКА, ЧАСТЬ 3
+
 let numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
 let personalMovieDB = {
@@ -54,17 +102,30 @@ let personalMovieDB = {
 	privat: false,
 };
 
-let a = prompt("Один из последних просмотренных фильмов?", "");
-let b = +prompt("На сколько оцените его?", "");
-let c = prompt("Один из последних просмотренных фильмов?", "");
-let d = +prompt("На сколько оцените его?", "");
+if (personalMovieDB.count < 11) {
+	alert("Просмотрено мало фильмов!");
+} else if (personalMovieDB.count > 10 && personalMovieDB.count < 31) {
+	alert("Вы классный зритель!");
+} else if (personalMovieDB.count > 30) {
+	alert("Да вы киноман!");
+} else {
+	alert("Ошибка!");
+}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+	let a = prompt("Один из последних просмотренных фильмов?", "");
+	let b = +prompt("На сколько оцените его?", "");
+    
+	if (a == "" || b == "" || a == null || b == null || a.length > 50 || b.length > 50) {
+		i--;
+	} else {
+		personalMovieDB.movies[a] = b;
+	}
+}
+
+// Условия в цикле выше можно прописать от обратного (с помощью оператора "Не" - "!")
 
 console.log(personalMovieDB);
-
-
 
 
 
